766b1cc6050121e8d48c8c2f23b6578df77e8e1f
Bruno Quintas <bquintas@amazon.com>
2023-10-12 18:08:29 +0200

    Added backup functionality (#2)
    * Added backup functionality
    
    * Update functions/backup-function/app.py
    
    Co-authored-by: Dan Johns <60505442+danjhd@users.noreply.github.com>
    
    * readme update
    
    ---------
    
    Co-authored-by: Dan Johns <60505442+danjhd@users.noreply.github.com>

---
+++ functions/backup-function/app.py
@@ -0,0 +1,37 @@
+import boto3
+import json
+import logging
+import os
+import time
+import shutil
+from botocore.exceptions import ClientError
+
+
+def lambda_handler(event, context):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    shutil.make_archive(f'/tmp/{timestr}', 'zip', '/mnt/efs')
+    upload_file(f"/tmp/{timestr}.zip", f"{os.environ['BACKUP_BUCKET']}")
+    return None
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
