f6f995f1131a2dbfe483f450d77cf7ac11f442e3
Dan Johns <danjhd@amazon.com>
2023-10-13 09:51:39 +0100

    Clearup after PR

--- functions/backup-function/app.py
+++ functions/backup-function/app.py
@@ -1,20 +1,23 @@
-import boto3
-import json
 import logging
 import os
-import time
 import shutil
+import time
+
+import boto3
 from botocore.exceptions import ClientError
 
 
-def lambda_handler(event, context):
+# pylint: disable=unused-argument
+def handler(event, context):
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    shutil.make_archive(f'/tmp/{timestr}', 'zip', '/mnt/efs')
-    upload_file(f"/tmp/{timestr}.zip", f"{os.environ['BACKUP_BUCKET']}")
+    shutil.make_archive(f"/tmp/{timestr}", "zip", "/mnt/efs")
+    upload_file(
+        f"/tmp/{timestr}.zip", os.environ["BACKUP_BUCKET"], os.environ["BACKUP_PREFIX"]
+    )
     return None
 
 
-def upload_file(file_name, bucket, object_name=None):
+def upload_file(file_name, bucket, prefix, object_name=None):
     """Upload a file to an S3 bucket
 
     :param file_name: File to upload
@@ -28,9 +31,9 @@ def upload_file(file_name, bucket, object_name=None):
         object_name = os.path.basename(file_name)
 
     # Upload the file
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
+        s3_client.upload_file(file_name, bucket, os.path.join(prefix, object_name))
     except ClientError as e:
         logging.error(e)
         return False
